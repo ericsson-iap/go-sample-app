0a2468d13d3b8be7cdbe426189895f441f3ac67d
adam-kiernan <adam.kiernan@ericsson.com>
2025-08-08 16:40:33 +0100

    Removed unused metrics (#13)

--- src/internal/metric/metric_test.go
+++ src/internal/metric/metric_test.go
@@ -11,17 +11,11 @@ func TestCreateMetrics(t *testing.T) {
 	t.Parallel()
 
 	assert.Nil(t, metric.RequestsTotal)
-	assert.Nil(t, metric.RequestsFailedTotal)
-	assert.Nil(t, metric.HelloWorldHTTPRequestsTotal)
 
 	metric.SetupMetrics()
 
 	assert.NotNil(t, metric.RequestsTotal,
 		"RequestsTotal has not been initialized")
-	assert.NotNil(t, metric.RequestsFailedTotal,
-		"RequestsFailedTotal has not been initialized")
-	assert.NotNil(t, metric.HelloWorldHTTPRequestsTotal,
-		"HelloWorldHTTPRequestsTotal has not been initialized")
 }
 
 func TestRegisterMetrics(t *testing.T) {
@@ -31,5 +25,5 @@ func TestRegisterMetrics(t *testing.T) {
 
 	metrics, err := metric.Registry.Gather()
 	assert.NoError(t, err)
-	assert.Len(t, metrics, 2)
+	assert.Len(t, metrics, 1)
 }
