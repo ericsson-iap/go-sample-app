4c0de2d6d00d4e912db8a5e10f16180099ca140e
eadakie <adam.kiernan@ericsson.com>
2025-08-08 13:48:36 +0100

    Removed unused metrics

--- src/internal/metric/metric.go
+++ src/internal/metric/metric.go
@@ -13,10 +13,6 @@ var (
 	Registry = prometheus.NewRegistry()
 	// RequestsTotal total number of API requests
 	RequestsTotal prometheus.Counter
-	// RequestsFailedTotal total number of API request failures
-	RequestsFailedTotal prometheus.Counter
-	// HelloWorldHTTPRequestsTotal total number of HTTP responses by status codes
-	HelloWorldHTTPRequestsTotal *prometheus.CounterVec
 )
 
 func createMetrics() {
@@ -26,25 +22,10 @@ func createMetrics() {
 			Name:      "requests_total",
 			Help:      "Total number of API requests",
 		})
-	RequestsFailedTotal = prometheus.NewCounter(
-		prometheus.CounterOpts{
-			Namespace: servicePrefix,
-			Name:      "requests_failed_total",
-			Help:      "Total number of API requests failures",
-		})
-	HelloWorldHTTPRequestsTotal = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Namespace: servicePrefix,
-			Name:      "hello_world_http_requests_total",
-			Help:      "Total number of HTTP responses by status codes",
-		},
-		[]string{"code"})
 }
 
 func registerMetrics() {
 	Registry.Register(RequestsTotal)               //nolint:errcheck // handling invalid metrics descriptors is outside the app scope
-	Registry.Register(RequestsFailedTotal)         //nolint:errcheck // handling invalid metrics descriptors is outside the app scope
-	Registry.Register(HelloWorldHTTPRequestsTotal) //nolint:errcheck // handling invalid metrics descriptors is outside the app scope
 }
 
 // SetupMetrics sets up the metrics
